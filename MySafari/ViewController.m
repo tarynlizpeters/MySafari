0910d056572d8d0b0424f8167d1f7dfdac84f3ce
Danny Vasquez <danny@Dannys-MacBook-Pro.local>
2016-01-12 18:36:45 -0600

    new changes

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -25,6 +25,16 @@
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if([textField.text containsString:@"http://www."] == YES) {
+        textField.text = textField.text;
+    }else if ([textField.text containsString:@"https://www."] == YES) {
+        textField.text = textField.text;
+    }else if ([textField.text containsString:@"www."] == YES) {
+        textField.text = [NSString stringWithFormat:@"http://%@", textField.text];
+    }else {
+        textField.text = [NSString stringWithFormat:@"http://www.%@", textField.text];
+    }
+
     NSURL *url = [NSURL URLWithString:textField.text];
     NSURLRequest *destination = [NSURLRequest requestWithURL:url];
     [self.WebView loadRequest:destination];
@@ -37,16 +47,23 @@
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView {
     [self.Spinner stopAnimating];
-    
 }
 
 - (IBAction)onBackButtonPressed:(UIButton *)sender {
     [self.WebView goBack];
+    
 }
 - (IBAction)onForwardButtonPressed:(UIButton *)sender {
     [self.WebView goForward];
     
 }
 
+- (IBAction)onStopLoadingButtonPressed:(UIButton *)sender {
+    [self.WebView stopLoading];
+}
+
+- (IBAction)onReloadButtonPressed:(UIButton *)sender {
+    [self.WebView reload];
+}
 
 @end
